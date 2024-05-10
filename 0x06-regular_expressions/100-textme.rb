ee00b964c198380bb42b7b7033837d589330d446
Megz259 <megan.hodsonrsa@gmail.com>
2024-05-10 02:23:56 -0700

    Last task update

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,5 @@
 #!/usr/bin/env ruby
 #advanced task
-puts ARGV[0].scan(/[from:(.*?)]\[to:(.*?)]\[flags(.*?)]/).join(",")
+phone = (\+\d{1,3}\(?\d{3}\)\(?\d{3}\))
+name = ([A-Za-z\']+)
+puts ARGV[0].scan(/[from:(name)(phone)]\[to:(name)(phone)]\[flags:(.*?)]/).join(",")
